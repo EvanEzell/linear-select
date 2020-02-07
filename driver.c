8000f79678dd60276467c83a456e9369036b840e
Evan C. Ezell <evanezell595@gmail.com>
2020-02-07 18:57:47 -0500

    Changed error msgs to print to stderr

--- driver.c
+++ driver.c
@@ -3,12 +3,12 @@
 #include "linear_select.h"
 
 void usage() {
-    printf("usage: linear_lselect input_file k n\n");
-    printf("       input_file - file to be sorted, must contain ints ");
-    printf("separated by a space\n");
-    printf("       k - the kth smallest element to be lselected, must ");
-    printf("less than or equal to n\n");
-    printf("       n - the number of elements to read from file\n");
+    fprintf(stderr,"usage: linear_lselect input_file k n\n");
+    fprintf(stderr,"       input_file - file to be sorted, must contain ints ");
+    fprintf(stderr,"separated by a space\n");
+    fprintf(stderr,"       k - the kth smallest element to be lselected, must ");
+    fprintf(stderr,"less than or equal to n\n");
+    fprintf(stderr,"       n - the number of elements to read from file\n");
 }
 
 int main (int argc, char **argv) {
@@ -22,22 +22,22 @@ int main (int argc, char **argv) {
     }
     if (sscanf(argv[3], "%d", &n) == 0 || sscanf(argv[2], "%d", &k) == 0)
     {
-        printf("n and k must be integers\n");
+        fprintf(stderr,"n and k must be integers\n");
         usage();
         exit(1);
     }
     if (n >= 1000000 || n < 1) {
-        printf("n must be between 1 and 1,000,000\n");
+        fprintf(stderr,"n must be between 1 and 1,000,000\n");
         usage();
         exit(1);
     }
     if (k < 1 || k > n) {
-        printf("k must be between 1 and n\n");
+        fprintf(stderr,"k must be between 1 and n\n");
         usage();
         exit(1);
     }
     if (!(input = fopen(argv[1], "r"))) {
-        printf("file could not be opened\n");
+        fprintf(stderr,"file could not be opened\n");
         usage();
         exit(1);
     }
