9ea4dd36f18646773f3df35979e762b522cabecb
Evan C. Ezell <evanezell595@gmail.com>
2020-02-07 18:44:04 -0500

    Adding other files

---
+++ linear_select.h
@@ -0,0 +1,5 @@
+void swap(int *x, int *y);
+int partition(int *arr, int left, int right, int pivot);
+int partition5(int *arr, int left, int right);
+int lselect(int *arr, int left, int right, int k);
+int pivot(int *arr, int left, int right);
